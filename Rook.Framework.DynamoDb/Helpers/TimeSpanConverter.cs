c7780c57043dff2f4b38c5779013050b5858a1e5
markjones <mark.jones@mab.org.uk>
2019-10-27 17:35:23 +0000

    adding property converter for TimeSpan objects so that they can be re-populated when read out of the database.

--- Rook.Framework.DynamoDb/Helpers/TimeSpanConverter.cs
+++ Rook.Framework.DynamoDb/Helpers/TimeSpanConverter.cs
@@ -2,7 +2,7 @@ using System;
 using Amazon.DynamoDBv2.DataModel;
 using Amazon.DynamoDBv2.DocumentModel;
 
-namespace Rook.Framework.DynamoDb.PropertyConverters
+namespace Rook.Framework.DynamoDb.Helpers
 {
     public class TimeSpanConverter : IPropertyConverter
     {
